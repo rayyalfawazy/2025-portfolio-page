c222712dc692c0e1f823b4d18e47bbcd86bf7007
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2025-03-06 14:14:36 +0700

    add social media button layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useTypewriter, Cursor} from 'react-simple-typewriter';
+import { FaLinkedin } from "react-icons/fa";
+
 
 export default function App() {
   const [roles] = useTypewriter({
@@ -13,24 +15,36 @@ export default function App() {
   })
 
   return (
-    <div className='bg-tertiary text-white'>
-      <nav className='grid grid-cols-3 py-7 px-24'>
+    <div className='text-white'>
+      {/* Navigation Bar */}
+      <nav className='bg-secondary grid grid-cols-3 py-7 px-24'>
         <h1 className='text-xl font-light'>Rayyan <span className='text-primary'>Alfawazy</span></h1>
         <ul className='flex justify-center space-x-10 font-light'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
+          <li><a className='hover:text-primary duration-150' href="#home">Home</a></li>
+          <li><a className='hover:text-primary duration-150' href="#about">About</a></li>
+          <li><a className='hover:text-primary duration-150' href="#services">Services</a></li>
+          <li><a className='hover:text-primary duration-150' href="#portfolio">Portfolio</a></li>
+          <li><a className='hover:text-primary duration-150' href="#contact">Contact</a></li>
         </ul>
-        <br/>
+        <h1 className='place-self-end'>Contact Me</h1>
       </nav>
-      <header className='grid grid-cols-3 px-25'>
+
+      {/* Header */}
+      <header className='bg-secondary grid grid-cols-3 px-25'>
         <div className='place-content-center space-y-2'>
           <h1 className='font-bold text-4xl'>Hi Everyone 👋</h1>
           <h2 className='text-2xl font-light'>My name is <span className='text-primary'>Rayyan Alfawazy Adheswarna</span></h2>
           <h2 className='text-xl'>I am a <span className='text-primary'>{roles}<Cursor/></span></h2>
-          <p className='font-light text-md text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa atque reiciendis odit. Sit ducimus laborum quidem sunt quas perspiciatis ratione rem, nulla, nobis provident perferendis debitis officiis! Quibusdam numquam odit ea dolore quo perspiciatis quae, deleniti soluta illum, repellat atque aut alias assumenda pariatur maiores quis nisi. Autem, eos ipsa.</p>
+          <p className='font-light text-md text-justify'>An enthusiastic coder in <span className='text-primary'>web and game development</span> from South Tangerang, Indonesia. I love learning new things. I also have expertise in making voice-overs with the quality of voice that I have.</p>
+          <div className=' rounded-lg w-full drop-shadow-3xl shadow-secondary text-black bg-tertiary p-5'>
+          <ul className='flex space-x-3 text-primary'>
+            <li><FaLinkedin /></li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>GitHub</li>
+
+          </ul>
+        </div>
         </div>
 
         <div className='h-[500px] col-span-2 overflow-hidden relative'>
